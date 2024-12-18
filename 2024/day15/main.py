69d03e6bd2db5fe33154bd22648f8adbe6147e34
unknown <barlimanbutterbur9@gmail.com>
2024-12-18 16:59:27 -0500

    completed solution to part 1 of day 15

--- 2024/day15/main.py
+++ 2024/day15/main.py
@@ -1,10 +1,3 @@
-# char_map = {
-#     '@' : 0,  # robot
-#     '.' : 1,  # empty
-#     'O' : 2,  # box
-#     '#' : 3,  # wall
-# }
-
 velocity_map = {
     '^' : (-1,  0),
     'v' : (+1,  0),
@@ -12,7 +5,9 @@ velocity_map = {
     '<' : ( 0, -1),
 }
 
-with open("test2.txt", 'r') as infile:
+##### Part 1 #####
+
+with open("input.txt", 'r') as infile:
     grid, moves = infile.read().split('\n\n')
 
 grid = [list(row) for row in grid.split('\n')]
@@ -32,13 +27,9 @@ def print_grid(grid: list, robot: list) -> None:
     print('robot:', robot)
     print(result[:(ncols + 1) * robot[0] + robot[1]] + '@' + result[(ncols + 1) * robot[0] + robot[1] + 1:])
 
-
-##### Part 1 #####
-
-print_grid(grid, robot)
+# print_grid(grid, robot)
 
 for move in moves:
-    print('\nmove:', move)
     if not ((0 <= robot[0] + move[0] < nrows) and (0 <= robot[1] + move[1] < ncols)):
         pass  # robot is trying to exit the grid; do nothing
     elif grid[robot[0] + move[0]][robot[1] + move[1]] == '.':
@@ -51,10 +42,10 @@ for move in moves:
             if not ((0 <= to_check[0] < nrows) and (0 <= to_check[1] < ncols)):
                 outcome = 0
                 break
-            if grid[to_check[0]][to_check[1]] != '#':
+            elif grid[to_check[0]][to_check[1]] == '#':
                 outcome = 1
                 break
-            if grid[to_check[0]][to_check[1]] != '.':
+            elif grid[to_check[0]][to_check[1]] == '.':
                 outcome = 2
                 break
             to_check[0] += move[0]
@@ -69,4 +60,66 @@ for move in moves:
             robot[1] += move[1]
     elif grid[robot[0] + move[0]][robot[1] + move[1]] == '#':
         pass  # robot is trying to push a wall; do nothing
-    print_grid(grid, robot)
+
+# print_grid(grid, robot)
+
+total_score = 0
+for row in range(len(grid)):
+    for col in range(len(grid[row])):
+        if grid[row][col] == 'O':
+            total_score += 100 * row + col
+
+print(f"Part 1: The sum of the boxes' GPS coordinates is {total_score}")
+
+
+##### Part 2 #####
+
+with open("test1.txt", 'r') as infile:
+    unscaled_grid, moves = infile.read().split('\n\n')
+    
+# Map move symbols to move vectors
+moves = [velocity_map[sym] for sym in moves if sym != '\n']
+
+# Scale up grid
+unscaled_grid = [
+    list(row)
+    for row in unscaled_grid.split('\n')
+]
+grid = [
+    [None] * (2 * len(unscaled_grid[i]))
+    for i in range(len(unscaled_grid))
+]
+robot = [None, None]
+for i in range(len(unscaled_grid)):
+    for j in range(len(unscaled_grid[i])):
+        if unscaled_grid[i][j] == '#':
+            grid[i][2 * j] = '#'
+            grid[i][2 * j + 1] = '#'
+        elif unscaled_grid[i][j] == '.':
+            grid[i][2 * j] = '.'
+            grid[i][2 * j + 1] = '.'
+        elif unscaled_grid[i][j] == 'O':
+            grid[i][2 * j] = '['
+            grid[i][2 * j + 1] = ']'
+        elif unscaled_grid[i][j] == '@':
+            grid[i][2 * j] = '.'
+            grid[i][2 * j + 1] = '.'
+            robot[0] = i
+            robot[1] = 2 * j
+
+nrows, ncols = len(grid), len(grid[0])
+
+print_grid(grid, robot)
+
+for move in moves:
+    if not ((0 <= robot[0] + move[0] < nrows) and (0 <= robot[1] + move[1] < ncols)):
+        pass  # robot is trying to exit the grid; do nothing
+    elif grid[robot[0] + move[0]][robot[1] + move[1]] == '#':
+        pass  # robot is trying to push a wall; do nothing
+    elif grid[robot[0] + move[0]][robot[1] + move[1]] == '.':
+        robot[0] += move[0]
+        robot[1] += move[1]
+    elif grid[robot[0] + move[0]][robot[1] + move[1]] == '[':
+        pass
+    elif grid[robot[0] + move[0]][robot[1] + move[1]] == ']':
+        pass
